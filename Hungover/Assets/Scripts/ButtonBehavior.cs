60c55fe72c02daad47c51bc412c6c9167f3654cb
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2015-10-07 12:33:08 -0400

    Create main menu and first party scene, create loading image which shows when scene is changing. Press ESC to return to main menu

---
+++ Hungover/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonBehavior : MonoBehaviour {
+
+    public GameObject loadingImage;
+
+    public void LoadOnClick(int level)
+    {
+        this.loadingImage.SetActive(true); //show loading image
+
+        Application.LoadLevel(level);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
