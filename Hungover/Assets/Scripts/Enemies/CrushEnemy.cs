b6f99a5d1ebc1b8166d911ae791cb100f93b1c19
Mark Massoud <markmssd@gmail.com>
2015-11-02 14:32:18 -0500

    Added Ash's Crush enemy, which changes direction every 2 seconds

---
+++ Hungover/Assets/Scripts/Enemies/CrushEnemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrushEnemy : BaseEnemy {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	void MoveCharacter () {
+		Utils.Print ("Overriden and not moving!");
+	}
+}
