6393df591005bdde7943abff94eaf82523ded52b
Mark Massoud <markmssd@gmail.com>
2015-11-03 03:37:29 -0500

    Added Run ability with appropriate animations, and deleted unnecessary files

--- Hungover/Assets/Scripts/Player.cs
+++ Hungover/Assets/Scripts/Player.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+
+	/***************************************
+	 * 
+	 *	Animation States:
+	 *	1 = Walk Up			5 = Run Up			9 = Dance Move 1
+	 *	2 = Walk Right		6 = Run Right		10 = Dance move 2
+	 *	3 = Walk Down		7 = Run Down		11 = Dance move 3
+	 *	4 = Walk Left		8 = Run Left		12 = Squat (Coming Soon)
+	 *
+	 ****************************************/
+
 	[SerializeField]
     public float speed;
 
@@ -26,20 +37,27 @@ public class Player : MonoBehaviour
 		Vector2 direction = new Vector2(h, v);
 		transform.Translate(direction * speed * Time.deltaTime);
 
-		SetWalking(direction);	   
+		SetAnimation(direction);	   
     }
 
-    void SetWalking(Vector2 direction)
+    void SetAnimation(Vector2 direction)
     {
-		int directionIdx = 0;
+		int animationIdx = 0;
 
-		if(direction.y > 0.0f) directionIdx = 1;		// Up
-		else if(direction.x > 0.0f) directionIdx = 2;	// Right
-		else if(direction.y < 0.0f) directionIdx = 3;	// Down
-		else if(direction.x < 0.0f) directionIdx = 4;	// Left
+		if(direction.y > 0.0f) animationIdx = 1;		// Up
+		else if(direction.x > 0.0f) animationIdx = 2;	// Right
+		else if(direction.y < 0.0f) animationIdx = 3;	// Down
+		else if(direction.x < 0.0f) animationIdx = 4;	// Left
+		else { mAnimator.Play("Character_Idle"); }
 
-		mAnimator.SetInteger("move_direction", directionIdx);
+		if(direction != Vector2.zero && Input.GetButton("Run")) {
+			animationIdx += 4;	// Will be in Run range
+			speed = 3;
+		} else {
+			speed = 2;
+		}
 
+		mAnimator.SetInteger("move_direction", animationIdx);
     }
 
 }
