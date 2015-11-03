8f012ea3025659da25239ba25b554faae66cd839
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2015-11-02 23:15:34 -0500

    Added menu and settings scenes (settings page is unfinished). Linked the different scenes. Imported some resources.

---
+++ Hungover/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonBehavior : MonoBehaviour {
+
+    public void LoadOnClick(int level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Application.LoadLevel("MainMenu");
+        }
+
+        if (Input.GetButtonDown("Submit") && Application.loadedLevel <= 1)
+        {
+            Application.LoadLevel("FroshParty");
+        }
+    }
+}
---
+++ Hungover/Assets/Scripts/SoundController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SoundController : MonoBehaviour {
+
+    private Slider slider;
+    private Toggle check;
+
+    /*
+    TODO: Configure audio controls
+    void Start()
+    {
+        this.slider = this.gameObject.GetComponentInChildren<Slider>();
+        this.check = this.gameObject.GetComponentInChildren<Toggle>();
+    }
+
+    void Update()
+    {
+        if(!this.check.isOn)
+        {
+            this.slider.enabled = false;
+        }
+
+        if(this.slider.value == 0)
+        {
+            this.check.isOn = false;
+        }
+    }
+
+    void toggleSliderActive()
+    {
+
+    }
+    */
+}
