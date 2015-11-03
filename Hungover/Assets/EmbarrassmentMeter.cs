cd9a7522e5350d520ef60e3adec3b695375a8086
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2015-11-03 04:06:53 -0500

    Embarrassment meter initial commit

--- Hungover/Assets/EmbarrassmentMeter.cs
+++ Hungover/Assets/EmbarrassmentMeter.cs
@@ -1,15 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class EmbarrassmentMeter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private Slider embarrassmentMeterSlider;
+
+    private float maximumCooldownTime = 10.0f;
+    private float cooldownTimer;
+    private float embarrassmentCooldownDecrement = 1.0f;
+
+    private void prepareState()
+    {
+        this.cooldownTimer = this.maximumCooldownTime;
+    }
+
+    void Start () {
+        this.prepareState();
+
+        this.embarrassmentMeterSlider = this.GetComponentInChildren<Slider>();
+    }
 	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+    private bool embarrassmentMeterFilled()
+    {
+        return this.embarrassmentMeterSlider.value >= this.embarrassmentMeterSlider.maxValue;
+    }
+
+    public void increaseEmbarrassment(float value)
+    {
+        if (this.embarrassmentMeterSlider.value + value >= this.embarrassmentMeterSlider.maxValue)
+        {
+            this.embarrassmentMeterSlider.value = this.embarrassmentMeterSlider.maxValue;
+        }
+        else
+        {
+            this.embarrassmentMeterSlider.value += value;
+        }
+    }
+
+    private void decreaseEmbarrassment(float value)
+    {
+        if (this.embarrassmentMeterSlider.value - value <= this.embarrassmentMeterSlider.minValue)
+        {
+            this.embarrassmentMeterSlider.value = this.embarrassmentMeterSlider.minValue;
+        }
+        else
+        {
+            this.embarrassmentMeterSlider.value -= value;
+        }
+    }
+
+    private void embarrassmentCooldown()
+    {
+        this.cooldownTimer -= Time.deltaTime;
+        if(this.cooldownTimer <= 0.0f)
+        {
+            this.decreaseEmbarrassment(this.embarrassmentCooldownDecrement);
+        }
+    }
+
 }
