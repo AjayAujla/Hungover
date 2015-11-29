8cffcbbc72bafc016da468820cf142da962e076b
ConfusedGiant <dorias.ahmed@gmail.com>
2015-11-29 16:12:23 -0500

    Sorted rooms (left to right) + fixed paths
    -Created a sorting function which sorts the rooms from left to right.
    -Fixed paths from room to room.

---
+++ Hungover/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorManager : MonoBehaviour {
+
+	void OnTriggerStay2D(Collider2D coll) {
+        
+		
+		if (coll.gameObject.tag == "OuterWall")
+		{
+			Destroy(coll.gameObject);
+		}  
+    }
+}
